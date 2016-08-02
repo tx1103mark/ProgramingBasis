407efd7be883f2eec7b976797d6bb8c6118bcef1
tx1103mark <tx1103mark@163.cm>
2016-08-02 18:38:14 +0800

    continue

---
+++ LeetCode/src/companyTest/Main.java
@@ -0,0 +1,20 @@
+package companyTest;
+
+import java.io.*;
+import java.util.*;
+class Test {
+}
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner cin = new Scanner(System.in);
+        int a, b;
+        while(cin.hasNextInt())
+        {
+            a = cin.nextInt();
+            b = cin.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
